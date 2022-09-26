3bc9887947b2901ef8d19dbd5034c6024ad39e81
mboswell-kp <MBoswell@kingandpartners.com>
2022-09-26 11:32:53 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Roman Numeral Converter</h1>
+    <p>Please input a number to convert into a roman numeral</p>
+    <RomanNumeralConverter />
+  </div>
+</template>
+
+<script>
+import RomanNumeralConverter from "@/components/RomanNumeralConverter";
+
+export default {
+  name: 'App',
+  components: {
+    RomanNumeralConverter
+  }
+}
+</script>
+
+<style>
+#app {
+  padding: 20%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/RomanNumeralConverter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="RomanNumeralConverter">
+    <form class="RomanNumeralConverter__form">
+      <div class="RomanNumeralConverter__field">
+        <label for="number">Number:</label>
+        <input type="number" id="number" name="number" v-model="number"/>
+      </div>
+    </form>
+    <div v-if="romanNumeral">
+      <h3>Output: <span>{{romanNumeral}}</span></h3>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import nomar from 'nomar';
+export default {
+  name: "RomanNumeralConverter",
+  data() {
+    return {
+      number: ''
+    }
+  },
+  computed: {
+    romanNumeral() {
+      if (this.number) {
+        return nomar(Number.parseInt(this.number));
+      }
+      return false;
+    }
+  }
+}
+</script>
+
+<style>
+.RomanNumeralConverter__field {
+  display: inline-flex;
+  flex-direction: column;
+}
+.RomanNumeralConverter__field label {
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+@media screen and (min-width: 750px) {
+  .RomanNumeralConverter__field {
+    flex-direction: row;
+  }
+  .RomanNumeralConverter__field label {
+    margin-bottom: 0;
+    margin-right: 20px
+  }
+}
+</style>
